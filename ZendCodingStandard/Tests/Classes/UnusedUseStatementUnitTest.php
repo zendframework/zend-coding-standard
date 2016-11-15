d60dba82ea940b013cd9ccffe477362f641c7dbd
webimpress <contact@webimpress.com>
2016-11-15 18:18:46 +0000

    Added tests for UnusedUseStatement sniff

---
+++ ZendCodingStandard/Tests/Classes/UnusedUseStatementUnitTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace ZendCodingStandard\Tests\Classes;
+
+use ZendCodingStandard\Tests\TestCase;
+
+class UnusedUseStatementUnitTest extends TestCase
+{
+    public function getErrorList()
+    {
+        return [];
+    }
+
+    public function getWarningList()
+    {
+        return [
+            6 => 1,
+            11 => 1,
+            13 => 1,
+            19 => 1,
+            20 => 1,
+            21 => 1,
+        ];
+    }
+}
