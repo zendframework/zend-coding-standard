7367ecdc8baa94bae547b0885d59845a9807e077
Geert Eltink <xtreamwayz@gmail.com>
2018-09-07 18:52:17 +0200

    Improve concatenation tests

--- test/fixable/concatenation-spacing.php
+++ test/fixable/concatenation-spacing.php
@@ -2,36 +2,24 @@
 
 declare(strict_types=1);
 
-$string = 'foo'.'bar';
-$string = 'foo'. 'bar';
-$string = 'foo' .'bar';
-$string = 'foo' . 'bar';
+$string = 'Hello'.$there.'. How are'.$you.$going.   "today $okay";
+$string = 'Hello' . $there . '. How are' . $you . $going . "today $okay";
+$string = 'Hello'.$there;
+$string = 'Hello'. $there;
+$string = 'Hello' .$there;
 
 $foo = 'foo';
 $bar    = 'bar';
-$string   = $foo.$bar;
-$string   = $foo. $bar;
-$string   = $foo .$bar;
-$string   = $foo . $bar;
-
-$string = 'foo'.$bar;
-$string = 'foo'. $bar;
-$string = 'foo' .$bar;
-$string = 'foo' . $bar;
-
-$string = $foo.'bar';
-$string = $foo. 'bar';
-$string = $foo .'bar';
-$string = $foo . 'bar';
-$string = $foo   .   'bar';
 
 $string = $foo . $bar. 'baz' .'quux'.$string;
-
 $string.=$foo;
 $string .= $foo;
-$string .=$foo;
+$string  .=$foo;
 
-$string = $foo .
-    $bar .
-    'baz'
-    .$string;
+$string = '1'
+          . '2'
+      . '3';
+
+$string = '1' .
+          '2' .
+          '3';
