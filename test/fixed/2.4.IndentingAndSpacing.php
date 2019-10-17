9787e730042432a729a118de0c50f2d4071b001b
Geert Eltink <geert.eltink@gmail.com>
2019-10-17 13:29:59 +0200

    Add section about encapsed strings and the the concatenation operator

---
+++ test/fixed/2.4.IndentingAndSpacing.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ZendCodingStandardTest\fixed;
+
+class IndentingAndSpacing
+{
+    public function testWhiteSpaceScopeIndent(): void
+    {
+        // Code MUST use an indent of 4 spaces for each indent level, and MUST
+        // NOT use tabs for indenting.
+
+        $foo = 'bar';
+    }
+
+    public function testEncapsedStrings(): void
+    {
+        $string = "Hello There\n";
+        $string = "Hello $there";
+    }
+
+    public function testConcatenationSpacing(): void
+    {
+        $string = 'Hello' . $there . '. How are' . $you . $doing . "today $okay";
+        $string = 'Hello' . $there . '. How are' . $you . $doing . "today $okay";
+        $string = 'Hello' . $there;
+        $string = 'Hello' . $there;
+        $string = 'Hello' . $there;
+
+        $foo = 'foo';
+        $bar = 'bar';
+
+        $string  = $foo . $bar . 'baz' . 'quux' . $string;
+        $string .= $foo;
+        $string .= $foo;
+        $string .= $foo;
+
+        $string = '1'
+            . '2'
+            . '3';
+
+        $string = '1'
+            . '2'
+            . '3';
+    }
+
+    public function testUnnecessaryStringConcat(): void
+    {
+        $x = 'My ' . 'string';
+        $x = 'My ' . 1234;
+        $x = 'My ' . $y . ' test';
+    }
+}
